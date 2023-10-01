9c4c4a05ee32a17190b8e24f11ea8771789e3c75
Heejin Son <chunsa5224@gmail.com>
2023-10-02 00:22:00 +0900

    initial setting

--- src/main/java/com/jeonse/SpringbootApplication.java
+++ src/main/java/com/jeonse/SpringbootApplication.java
@@ -3,11 +3,32 @@ package com.jeonse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.*;
+
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+
+		/*try {
+			Connection conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306",
+					"heejin",
+					"test1234"
+			);
+
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(
+					"show databases"
+			);
+
+			while (rs.next()) {
+				System.out.println(rs.getString(1));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}*/
 	}
 
 }
---
+++ src/main/java/com/jeonse/controller/bootController.java
@@ -0,0 +1,18 @@
+package com.jeonse.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class bootController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        log.info("username={}, age={}", "memberName","memberAge");
+        log.info("vaild Ok");
+
+        return "ok";
+    }
+}
