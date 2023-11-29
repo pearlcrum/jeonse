b8b3d6051a6a4d97206094b96337662ae33d589d
Heejin Son <chunsa5224@gmail.com>
2023-11-29 14:24:18 +0900

    Commonchecklist 라디오버튼 thymeleaf 일부 적용(완) login시 세션에 memID 값 넘기기(완)

--- src/main/java/com/jeonse/config/SecurityController.java
+++ src/main/java/com/jeonse/config/SecurityController.java
@@ -8,18 +8,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class SecurityController {
 
     @Autowired
     MemberService memberService;
 
-    @RequestMapping("/main")
-    public String home(@AuthenticationPrincipal UserDetails userInfo, Model model) {
+    @RequestMapping("/")
+    public String home(@AuthenticationPrincipal UserDetails userInfo, HttpServletRequest httpServletRequest, Model model) {
         if(userInfo!=null) {
             model.addAttribute("user", memberService.getUserInfo(userInfo.getUsername()));
 
         }
+        //세션을 생성하기 전에 기존의 세션 파기
+        httpServletRequest.getSession().invalidate();
+        HttpSession session = httpServletRequest.getSession(true); //세션이 없으면 생성
+
+        session.setAttribute("memID", userInfo.getUsername());
+        System.out.println("Security Controller " + userInfo.getUsername());
+        session.setMaxInactiveInterval(1800); //session이 30분동안 유지됨.
+
         return "/index";
     }
 }
--- src/main/java/com/jeonse/controller/CommonchecklistController.java
+++ src/main/java/com/jeonse/controller/CommonchecklistController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
@@ -22,19 +23,51 @@ public class CommonchecklistController {
     @Autowired
     private CommonchecklistService commonchecklistService;
 
+
     //체크리스트 보여주기
     @GetMapping("/checklist")
-    public String checklist(HttpServletRequest request) throws Exception{
+    public String checklist(@SessionAttribute(name="memID", required = false) String memID,  CommonchecklistDTO commonchecklistDTO) {
+        //체크리스트에 값이 이미 있는 경우
+        if(commonchecklistService.checkCommonchecklistID(memID)!=0){
+            //세션 초기화 및 profile로 이동하여 기존 체크리스트 삭제 요청 필요 (-->해야해?)
+            //return "profile";
+        }else{
+            //체크리스트에 값이 없는 경우
+            //memid 를 받아야함.
+            System.out.println("memID is " + memID);
+        }
+        return "checklist";
+    }
+
+    @PostMapping("/checklist")
+    public String checklistSubmit(CommonchecklistDTO commonchecklistDTO) {
+        System.out.println("@@@"+  commonchecklistDTO.getLandlordPossessionMonth());
+        commonchecklistService.insertCommonchecklist(commonchecklistDTO);
+        return "redirect/main";
+    }
+    /*@PostMapping("/checklistNextStep")
+    public String checklistNextStep(@RequestParam("checklistNextStep") CommonchecklistDTO checklistDto ) {
+        System.out.println("여기는 잘 들어 오나요?");
+        commonchecklistService.insertCommonchecklist(checklistDto);
+        return "redirect:/index";
+    }*/
+
+
+    /*@GetMapping("/checklist")
+    //public String checklist(HttpServletRequest request) throws Exception{
+    public String checklist(CommonchecklistDTO commonchecklistDTO) {
         //체크리스트에 값이 이미 있는 경우
         //세션 초기화 및 profile로 이동하여 기존 체크리스트 삭제 요청 필요
         //체크리스트에 값이 없는 경우
 
-        HttpSession session = request.getSession();
+        //HttpSession session = request.getSession();
+
+        System.out.println("@@@@" + commonchecklistDTO.getLandlordPossessionMonth());
         //session.setAttribute("memID",memID); //세션에 현재 들어온 사람 정보 넣기.
         return "checklist";
-    }
+    }*/
     //체크리스트 값 입력
-    @PostMapping("/checklistNextStep")
+    /*@PostMapping("/checklistNextStep")
     public String checklistNextStep(@RequestParam("checklistNextStep") CommonchecklistDTO checklistDto, HttpServletRequest request) {
         System.out.println("여기는 잘 들어 오나요?");
         HttpSession session=request.getSession();
@@ -44,7 +77,7 @@ public class CommonchecklistController {
         //session.setAttribute(); //여기에 넣자
         commonchecklistService.insertCommonchecklist(checklistDto);
         return "redirect:/index";
-    }
+    }*/
     public String processSignup(@RequestParam("inputName") String inputName,
                                 @RequestParam("inputEmail") String inputEmail,
                                 @RequestParam("inputPassword") String inputPassword,
